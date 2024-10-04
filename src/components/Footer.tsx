0a11c97d8eb467a169aa017b6f36ca3684e05f36
Michell Zappa <mz@envisioning.io>
2024-10-04 18:05:30 +0200

    refactor: Remove DarkModeToggle component and update Footer component
    This commit removes the DarkModeToggle component, which is no longer needed. The functionality of toggling between light and dark mode has been integrated directly into the Footer component. The Footer component has been updated to include a link to the Envisioning website and display the current year. This simplifies the codebase and improves the user experience by providing a consistent dark mode toggle in the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
-import DarkModeToggle from "./DarkModeToggle";
-
 const Footer = () => {
   return (
     <footer className="mt-auto py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <p className="text-sm">Envisioning (2024)</p>
-        <DarkModeToggle />
+      <div className="container mx-auto flex items-left">
+        <a href="https://www.envisioning.io" className="text-sm mr-2">
+          Envisioning
+        </a>
+        <span className="text-sm text-gray-500 dark:text-gray-400 mr-8">
+          {new Date().getFullYear()}
+        </span>
       </div>
     </footer>
   );
